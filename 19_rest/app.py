e5c59750a7faa969fa195e61b8dcdf8f13336b53
anna <anya.zorin@gmail.com>
2021-04-07 03:31:49 -0400

    k19

---
+++ 19_rest/app.py
@@ -0,0 +1,23 @@
+# Team e&a - Anya Zorin, Ethan Shenker
+# SoftDev
+# K19 -- A RESTful Journey Skyward
+# 2021-4-5
+
+from flask import Flask, render_template
+import urllib, json
+app = Flask(__name__) 
+
+file=open("key_nasa.txt", "r")
+url="https://api.nasa.gov/planetary/apod?api_key="+file.read()
+file.close()
+
+@app.route("/")
+def root():
+    u=urllib.request.urlopen(url)
+    response=u.read()
+    data=json.loads(response)
+    return render_template("main.html", title=data["title"], pic=data['url'])
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run()
