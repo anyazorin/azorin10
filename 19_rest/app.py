71de1a9f31d2cbe0d1aa9441d6d8098cc85e922c
anna <anya.zorin@gmail.com>
2021-04-07 09:27:13 -0400

    k19

--- 19_rest/app.py
+++ 19_rest/app.py
@@ -16,7 +16,7 @@ def root():
     u=urllib.request.urlopen(url)
     response=u.read()
     data=json.loads(response)
-    return render_template("main.html", title=data["title"], pic=data['url'])
+    return render_template("main.html", title=data["title"], pic=data['url'], text=data['explanation'])
 
 if __name__ == "__main__":
     app.debug=True
