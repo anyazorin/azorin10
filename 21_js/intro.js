35a7145e4b0fdd047b9ac78c3260196234987498
anna <anya.zorin@gmail.com>
2021-04-09 10:25:24 -0400

    k21

---
+++ 21_js/intro.js
@@ -0,0 +1,21 @@
+// Team e&a - Anya Zorin, Ethan Shenker
+// SoftDev
+// K19 -- A RESTful Journey Skyward
+// 2021-4-5
+function factI(n){
+    if (n==0){return 1}//base case/exception of factorial is 0!=1
+    var ans=1;
+    for (i=1; i<=n;i++){//simple for loop
+        ans=i*ans;
+    }
+    return ans;
+}
+
+function factR(n) {
+    if (n==0){return 1}//base case
+    return n* factR(n-1);
+}
+//js has no print, so use console log
+//if on VS code, install package "Code Runner" then ctrl+alt+n prints console log in output
+console.log(factI(9));
+console.log(factR(9));
