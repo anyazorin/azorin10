656e1a04f8e7532d1bbb2da55c3e3535852a19d1
anna <anya.zorin@gmail.com>
2021-04-13 08:04:25 -0400

    k21

--- 21_js/intro.js
+++ 21_js/intro.js
@@ -19,3 +19,33 @@ function factR(n) {
 //if on VS code, install package "Code Runner" then ctrl+alt+n prints console log in output
 console.log(factI(9));
 console.log(factR(9));
+
+function fibI(n) {
+    if (n == 0) {return 0};
+    if (n == 1) {return 1};
+
+    var first = 0;
+    var second = 1;
+
+    for (i=0; i<n-1; i++) {
+        var temp = second;
+        second += first;
+        first = temp;
+    }
+    return second;
+}
+
+console.log(fibI(1));
+console.log(fibI(4));
+console.log(fibI(8));
+
+
+function fibR(n) {
+    if (n == 0) {return 0};
+    if (n == 1) {return 1};
+    return fibR(n-1) + fibR(n-2);
+}
+
+console.log(fibR(1));
+console.log(fibR(4));
+console.log(fibR(8));
